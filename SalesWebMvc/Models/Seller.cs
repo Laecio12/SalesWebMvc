16a4d75d8c81bd9ce8b8cc06b3b4bb8c374493dd
Laecio12 <laecio12silva@gmail.com>
2021-11-10 11:41:17 -0300

    Added class Seller

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double baseSalary { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+    }
+}
